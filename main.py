b6f47562965f91fdb9606c559115f573d34f9c39
soundofnothing <144745171+soundofnothing@users.noreply.github.com>
2023-09-11 22:49:03 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,25 +1,41 @@
-from signature import normalize_text, calculate_character_frequencies, calculate_word_frequencies, calculate_stopword_and_nonletter_frequencies, chunk_text
+import graph
+import timeline
 
-# Normalize text
-text = "Hello, World!"
-normalized_text = normalize_text(text)
-print(normalized_text)
+def process_textual_data(rss_feed):
+    # Generate a stream of textual data from the RSS feed
+    timeseries = timeline.generate_timeline_timeseries(rss_feed)
 
-# Calculate character frequencies
-character_frequencies = calculate_character_frequencies(text)
-print(character_frequencies)
+    # Initialize a dictionary to store the graphs
+    graphs = {}
 
-# Calculate word frequencies
-word_frequencies = calculate_word_frequencies(text)
-print(word_frequencies)
+    # Process each entry in the timeseries
+    for entry in timeseries:
+        # Get the timestamped text from the entry
+        timestamp, text = entry
 
-# Calculate stopword and nonletter frequencies
-stopword_frequencies, nonletter_frequencies = calculate_stopword_and_nonletter_frequencies(text)
-print(stopword_frequencies)
-print(nonletter_frequencies)
+        # Create graphs from the text
+        character_graph = graph.create_character_graph(text)
+        word_graph = graph.create_word_graph(text)
+        stopword_punctuation_graph = graph.create_stopword_punctuation_graph(text)
 
-# Chunk text
-data = ["This is the first text.", "This is the second text."]
-num_tokens = 3
-chunks = chunk_text(data, num_tokens)
-print(chunks)
+        # Store the graphs in the dictionary under the corresponding timestamp
+        graphs[timestamp] = {
+            "character": character_graph,
+            "word": word_graph,
+            "stopword_punctuation": stopword_punctuation_graph
+        }
+
+    # Convert the graphs to numpy arrays
+    numpy_graphs = graph.convert_graphs_to_numpy(graphs)
+
+    # Perform topological data analysis on the numpy graphs
+    for timestamp, numpy_graph in numpy_graphs.items():
+        umap_embedding = graph.convert_numpy_to_umap(numpy_graph)
+        # Use the UMAP embedding for further analysis or visualization
+
+    # Add any additional processing or analysis steps here
+
+if __name__ == "__main__":
+    # Example usage
+    rss_feed = timeline.RSSFeed.ACCOUNT1
+    process_textual_data(rss_feed)
