f72ba3c97b42f6d33d632289906c84494218a29e
soundofnothing <144745171+soundofnothing@users.noreply.github.com>
2023-09-11 21:28:09 -0400

    Update graph.py

--- graph.py
+++ graph.py
@@ -1,8 +1,23 @@
 import networkx as nx
+import numpy as np
 from signature import calculate_character_frequencies, calculate_word_frequencies, calculate_stopword_and_nonletter_frequencies, normalize_text
+from umap import UMAP
 
 
-def create_character_graph(text: str):
+def convert_graph_to_numpy(graph: nx.DiGraph) -> np.ndarray:
+    node_indices = {node: index for index, node in enumerate(graph.nodes())}
+    adjacency_matrix = nx.to_numpy_matrix(graph, nodelist=graph.nodes())
+    return np.array(adjacency_matrix), node_indices
+
+
+def convert_graphs_to_numpy(graphs: dict) -> dict:
+    numpy_graphs = {}
+    for graph_name, graph in graphs.items():
+        numpy_graphs[graph_name] = convert_graph_to_numpy(graph)
+    return numpy_graphs
+
+
+def create_character_graph(text: str) -> nx.DiGraph:
     # Generate character frequencies from the fingerprinted text
     character_frequencies = calculate_character_frequencies(text)
 
@@ -16,7 +31,7 @@ def create_character_graph(text: str):
     return graph
 
 
-def create_word_graph(text: str):
+def create_word_graph(text: str) -> nx.DiGraph:
     # Generate word frequencies from the fingerprinted text
     word_frequencies = calculate_word_frequencies(text)
 
@@ -30,7 +45,7 @@ def create_word_graph(text: str):
     return graph
 
 
-def create_stopword_punctuation_graph(text: str):
+def create_stopword_punctuation_graph(text: str) -> nx.DiGraph:
     # Generate stopword and punctuation frequencies from the fingerprinted text
     stopword_frequencies, punctuation_frequencies = calculate_stopword_and_nonletter_frequencies(text)
 
@@ -48,6 +63,12 @@ def create_stopword_punctuation_graph(text: str):
     return graph
 
 
+def convert_numpy_to_umap(numpy_graph: tuple) -> np.ndarray:
+    adjacency_matrix, _ = numpy_graph
+    umap_embedding = UMAP(n_components=2, metric='precomputed').fit_transform(adjacency_matrix)
+    return umap_embedding
+
+
 # Example usage
 text = "This is a sample text for graph creation."
 normalized_text = normalize_text(text)
@@ -55,7 +76,19 @@ character_graph = create_character_graph(normalized_text)
 word_graph = create_word_graph(normalized_text)
 stopword_punctuation_graph = create_stopword_punctuation_graph(normalized_text)
 
-# Print graph information
-print("Character Graph nodes:", character_graph.nodes())
-print("Word Graph nodes:", word_graph.nodes())
-print("Stopword and Punctuation Graph nodes:", stopword_punctuation_graph.nodes())
+# Convert graphs to numpy arrays
+graphs = {
+    'character_graph': character_graph,
+    'word_graph': word_graph,
+    'stopword_punctuation_graph': stopword_punctuation_graph
+}
+numpy_graphs = convert_graphs_to_numpy(graphs)
+
+# Convert numpy arrays to UMAP embeddings
+umap_embeddings = {graph_name: convert_numpy_to_umap(numpy_graph) for graph_name, numpy_graph in numpy_graphs.items()}
+
+# Print the UMAP embeddings
+for graph_name, umap_embedding in umap_embeddings.items():
+    print(f"{graph_name} UMAP embedding:")
+    print(umap_embedding)
+    print()
