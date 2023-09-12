c9362335eb620a9eb4cbcf3b82bc9044d0131b87
soundofnothing <144745171+soundofnothing@users.noreply.github.com>
2023-09-11 23:00:37 -0400

    Update visualize.py

--- visualize.py
+++ visualize.py
@@ -1,47 +1,68 @@
-from datetime import datetime
 import plotly.graph_objects as go
 import plotly.express as px
-from signature import normalize_text, calculate_character_frequencies, calculate_word_frequencies, calculate_stopword_and_nonletter_frequencies
+from datetime import datetime
+from typing import Dict, List
 
 
-def visualize_character_frequencies(text: str, timestamp: datetime = None):
-    character_frequencies = calculate_character_frequencies(text)
+def visualize_character_frequencies(timeseries: Dict[datetime, Dict[str, int]]):
+    fig = go.Figure()
 
-    characters = list(character_frequencies.keys())
-    frequencies = list(character_frequencies.values())
+    for timestamp, character_frequencies in timeseries.items():
+        characters = list(character_frequencies.keys())
+        frequencies = list(character_frequencies.values())
 
-    fig = go.Figure(data=[go.Bar(x=characters, y=frequencies)])
-    fig.update_layout(title='Character Frequencies', xaxis_title='Character', yaxis_title='Frequency')
-    if timestamp:
-        fig.update_layout(title=f'Character Frequencies ({timestamp})')
+        fig.add_trace(go.Bar(
+            x=characters,
+            y=frequencies,
+            name=f'Character Frequencies ({timestamp})'
+        ))
+
+    fig.update_layout(title='Character Frequencies over Time', xaxis_title='Character', yaxis_title='Frequency')
     fig.show()
 
 
-def visualize_word_frequencies(text: str, timestamp: datetime = None):
-    word_frequencies = calculate_word_frequencies(text)
+def visualize_word_frequencies(timeseries: Dict[datetime, Dict[str, int]]):
+    fig = go.Figure()
 
-    words = list(word_frequencies.keys())
-    frequencies = list(word_frequencies.values())
+    for timestamp, word_frequencies in timeseries.items():
+        words = list(word_frequencies.keys())
+        frequencies = list(word_frequencies.values())
 
-    fig = px.bar(x=words, y=frequencies, labels={'x': 'Words', 'y': 'Frequency'})
-    if timestamp:
-        fig.update_layout(title=f'Word Frequencies ({timestamp})')
+        fig.add_trace(go.Bar(
+            x=words,
+            y=frequencies,
+            name=f'Word Frequencies ({timestamp})'
+        ))
+
+    fig.update_layout(title='Word Frequencies over Time', xaxis_title='Word', yaxis_title='Frequency')
     fig.show()
 
 
-def visualize_stopword_and_nonletter_frequencies(text: str, timestamp: datetime = None):
-    stopword_frequencies, nonletter_frequencies = calculate_stopword_and_nonletter_frequencies(text)
+def visualize_stopword_and_nonletter_frequencies(timeseries: Dict[datetime, Dict[str, int]]):
+    fig = go.Figure()
 
-    stopwords = list(stopword_frequencies.keys())
-    stopwords_frequencies = list(stopword_frequencies.values())
-    nonletter = list(nonletter_frequencies.keys())
-    nonletter_frequencies = list(nonletter_frequencies.values())
+    for timestamp, (stopword_frequencies, nonletter_frequencies) in timeseries.items():
+        stopwords = list(stopword_frequencies.keys())
+        stopwords_frequencies = list(stopword_frequencies.values())
+        nonletter = list(nonletter_frequencies.keys())
+        nonletter_frequencies = list(nonletter_frequencies.values())
 
-    fig = go.Figure(data=[
-        go.Bar(name='Stopwords', x=stopwords, y=stopwords_frequencies),
-        go.Bar(name='Non-letter Characters', x=nonletter, y=nonletter_frequencies)
-    ])
-    fig.update_layout(title='Stopword and Non-letter Character Frequencies', xaxis_title='Words/Characters', yaxis_title='Frequency')
-    if timestamp:
-        fig.update_layout(title=f'Stopword and Non-letter Character Frequencies ({timestamp})')
+        fig.add_trace(go.Bar(
+            name=f'Stopwords ({timestamp})',
+            x=stopwords,
+            y=stopwords_frequencies
+        ))
+
+        fig.add_trace(go.Bar(
+            name=f'Non-letter Characters ({timestamp})',
+            x=nonletter,
+            y=nonletter_frequencies
+        ))
+
+    fig.update_layout(
+        title='Stopword and Non-letter Character Frequencies over Time',
+        xaxis_title='Words/Characters',
+        yaxis_title='Frequency',
+        barmode='stack'
+    )
     fig.show()
