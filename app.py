e4878dd5e6724d8d91028328552f46c37cc15f45
soundofnothing <144745171+soundofnothing@users.noreply.github.com>
2023-09-12 00:25:39 -0400

    Update app.py

--- app.py
+++ app.py
@@ -4,6 +4,7 @@ import dash_html_components as html
 import graph
 import timeline
 import umap.plot
+from dash.dependencies import Input, Output
 from visualize import visualize_character_frequencies, visualize_word_frequencies, visualize_stopword_and_nonletter_frequencies
 
 
@@ -40,6 +41,12 @@ def visualize_umap_embedding(embedding: any) -> dcc.Graph:
     return dcc.Graph(figure=figure)
 
 
+def visualize_word_graph(word_graph: any) -> dcc.Graph:
+    # Example visualization using graph.create_word_graph
+    figure = graph.create_word_graph(word_graph)
+    return dcc.Graph(figure=figure)
+
+
 if __name__ == "__main__":
     # Example usage
     rss_feed = timeline.RSSFeed.ACCOUNT1
@@ -50,11 +57,25 @@ if __name__ == "__main__":
 
     # Set up Dash app layout
     app.layout = html.Div([
-        dcc.Graph(figure=visualize_character_frequencies(graphs)),
-        dcc.Graph(figure=visualize_word_frequencies(graphs)),
-        dcc.Graph(figure=visualize_stopword_and_nonletter_frequencies(graphs)),
-        visualize_umap_embedding(graph.convert_numpy_to_umap(graph.convert_graphs_to_numpy(graphs)))
+        dcc.Graph(id="character-frequencies", figure=visualize_character_frequencies(graphs)),
+        dcc.Graph(id="word-frequencies", figure=visualize_word_frequencies(graphs)),
+        dcc.Graph(id="stopword-nonletter-frequencies", figure=visualize_stopword_and_nonletter_frequencies(graphs)),
+        dcc.Graph(id="umap-embedding", figure=visualize_umap_embedding(graph.convert_numpy_to_umap(graph.convert_graphs_to_numpy(graphs))))
     ])
+
+    @app.callback(
+        Output("word-frequencies", "figure"),
+        [Input("character-frequencies", "hoverData")]
+    )
+    def update_word_graph(hoverData):
+        if hoverData is not None:
+            timestamp = hoverData["points"][0]["x"]
+            word_graph = graphs.get(timestamp, {}).get("word", None)
+
+            if word_graph is not None:
+                return visualize_word_graph(word_graph)
+
+        return None
 
     # Run the Dash app
     app.run_server(debug=True)
