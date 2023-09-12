6d56238f8b2ab4471889efd4e34a7a3beb085ae9
soundofnothing <144745171+soundofnothing@users.noreply.github.com>
2023-09-11 21:50:02 -0400

    Update graph.py

--- graph.py
+++ graph.py
@@ -1,7 +1,7 @@
 import networkx as nx
 import numpy as np
 from sklearn.feature_extraction.text import CountVectorizer, TfidfVectorizer
-from signature import calculate_character_frequencies, calculate_stopword_and_nonletter_frequencies, normalize_text
+from signature import calculate_character_frequencies, calculate_word_frequencies, calculate_stopword_and_nonletter_frequencies, normalize_text
 from umap import UMAP
 
 
@@ -27,6 +27,20 @@ def create_character_graph(text: str) -> nx.DiGraph:
     # Add nodes to the graph
     for character, frequency in character_frequencies.items():
         graph.add_node(character, frequency=frequency)
+
+    return graph
+
+
+def create_word_graph(text: str) -> nx.DiGraph:
+    # Generate word frequencies from the fingerprinted text
+    word_frequencies = calculate_word_frequencies(text)
+
+    # Create a directed graph
+    graph = nx.DiGraph()
+
+    # Add nodes to the graph
+    for word, frequency in word_frequencies.items():
+        graph.add_node(word, frequency=frequency)
 
     return graph
 
@@ -66,47 +80,3 @@ def convert_numpy_to_umap(numpy_graph: tuple) -> np.ndarray:
     adjacency_matrix, _ = numpy_graph
     umap_embedding = UMAP(n_components=2, metric='precomputed').fit_transform(adjacency_matrix)
     return umap_embedding
-
-
-# Example usage
-text = "This is a sample text for graph creation."
-normalized_text = normalize_text(text)
-
-# Create a graph from the Bag-of-Words representation
-count_vectorizer = CountVectorizer()
-count_matrix = count_vectorizer.fit_transform([normalized_text])
-bag_of_words_graph = create_graph_from_count_matrix(count_matrix)
-
-# Create a graph from the TF-IDF representation
-tfidf_vectorizer = TfidfVectorizer()
-tfidf_matrix = tfidf_vectorizer.fit_transform([normalized_text])
-tfidf_graph = create_graph_from_tfidf_matrix(tfidf_matrix)
-
-# Create character graph
-character_graph = create_character_graph(normalized_text)
-
-# Create stopword and punctuation graph
-stopword_punctuation_graph = create_stopword_punctuation_graph(normalized_text)
-
-# Convert graphs to numpy arrays
-graphs = {
-    'bag_of_words_graph': bag_of_words_graph,
-    'tfidf_graph': tfidf_graph,
-    'character_graph': character_graph,
-    'stopword_punctuation_graph': stopword_punctuation_graph
-}
-numpy_graphs = convert_graphs_to_numpy(graphs)
-
-# Convert numpy arrays to UMAP embeddings
-umap_embeddings = {graph_name: convert_numpy_to_umap(numpy_graph) for graph_name, numpy_graph in numpy_graphs.items()}
-
-# Print the graphs and UMAP embeddings
-print("Bag-of-Words Graph nodes:", bag_of_words_graph.nodes())
-print("TF-IDF Graph nodes:", tfidf_graph.nodes())
-print("Character Graph nodes:", character_graph.nodes())
-print("Stopword and Punctuation Graph nodes:", stopword_punctuation_graph.nodes())
-
-for graph_name, umap_embedding in umap_embeddings.items():
-    print(f"{graph_name} UMAP embedding:")
-    print(umap_embedding)
-    print()
