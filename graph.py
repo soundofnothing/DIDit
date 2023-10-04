4198ee2032cd2ef97a39a6157af0ffca2b58f51f
soundofnothing <144745171+soundofnothing@users.noreply.github.com>
2023-10-03 23:50:44 -0400

    Update graph.py
    WIP

--- graph.py
+++ graph.py
@@ -1,83 +1,56 @@
 import networkx as nx
 import numpy as np
 from sklearn.feature_extraction.text import CountVectorizer, TfidfVectorizer
-from signature import calculate_character_frequencies, calculate_word_frequencies, calculate_stopword_and_nonletter_frequencies, normalize_text
+from signature import Fingerprint
 from umap import UMAP
 
 
-def create_graph_from_count_matrix(count_matrix):
-    # Create a graph from the count matrix
-    graph = nx.from_scipy_sparse_matrix(count_matrix, create_using=nx.DiGraph)
-    return graph
-
-
-def create_graph_from_tfidf_matrix(tfidf_matrix):
-    # Create a graph from the tf-idf matrix
-    graph = nx.from_scipy_sparse_matrix(tfidf_matrix, create_using=nx.DiGraph)
-    return graph
-
-
-def create_character_graph(text: str) -> nx.DiGraph:
-    # Generate character frequencies from the fingerprinted text
-    character_frequencies = calculate_character_frequencies(text)
-
-    # Create a directed graph
-    graph = nx.DiGraph()
-
+def create_char_graph(text: str) -> nx.DiGraph:
+    fingerprint = Fingerprint.from_text(text)
+    char_graph = nx.DiGraph()
+    normed_char_graph = nx.DiGraph()
+    
     # Add nodes to the graph
-    for character, frequency in character_frequencies.items():
-        graph.add_node(character, frequency=frequency)
+    for character, frequency in fingerprint.NORMALIZED_CHARACTER_FREQUENCY.items():
+        normed_char_graph.add_node(character, frequency=frequency)
 
-    return graph
+    for character, frequency in fingerprint.CHARACTER_FREQUENCY.items():
+        char_graph.add_node(character, frequency=frequency)
+    
+    return nx.tensor_product(normed_char_graph, char_graph)
 
 
 def create_word_graph(text: str) -> nx.DiGraph:
-    # Generate word frequencies from the fingerprinted text
-    word_frequencies = calculate_word_frequencies(text)
-
-    # Create a directed graph
-    graph = nx.DiGraph()
-
+    fingerprint = Fingerprint.from_text(text)
+    word_graph = nx.DiGraph()
+    normed_word_graph = nx.DiGraph()
+    
     # Add nodes to the graph
-    for word, frequency in word_frequencies.items():
-        graph.add_node(word, frequency=frequency)
+    for word, frequency in fingerprint.WORD_FREQUENCY.items():
+        word_graph.add_node(word, frequency=frequency)
 
-    return graph
-
-
-def create_stopword_punctuation_graph(text: str) -> nx.DiGraph:
-    # Generate stopword and punctuation frequencies from the fingerprinted text
-    stopword_frequencies, punctuation_frequencies = calculate_stopword_and_nonletter_frequencies(text)
-
-    # Create a directed graph
-    graph = nx.DiGraph()
-
-    # Add stopword nodes to the graph
-    for stopword, frequency in stopword_frequencies.items():
-        graph.add_node(stopword, frequency=frequency, type='stopword')
-
-    # Add punctuation nodes to the graph
-    for punctuation, frequency in punctuation_frequencies.items():
-        graph.add_node(punctuation, frequency=frequency, type='punctuation')
-
-    return graph
+    for word, frequency in fingerprint.NORMALIZED_WORD_FREQUENCY.items():
+        normed_word_graph.add_node(word, frequency=frequency)
+    # todo investigate adding negative frequencies
+    return nx.tensor_product(normed_word_graph, word_graph)
 
 
-def convert_graph_to_numpy(graph: nx.DiGraph) -> np.ndarray:
-    node_indices = {node: index for index, node in enumerate(graph.nodes())}
-    adjacency_matrix = nx.to_numpy_array(graph, nodelist=graph.nodes())
-    return adjacency_matrix, node_indices
+def create_stopword_nonletter_graph(text: str) -> nx.DiGraph:
+    fingerprint = Fingerprint.from_text(text)
+    stopword_nonletter_graph = nx.DiGraph()
+
+    # Add nodes for stopwords and non-letter characters
+    for word, frequency in fingerprint.STOPWORD_FREQUENCY.items():
+        stopword_nonletter_graph.add_node(word, frequency=frequency)
+
+    for character, frequency in fingerprint.NONLETTER_FREQUENCY.items():
+        stopword_nonletter_graph.add_node(character, frequency=frequency)
+
+    return stopword_nonletter_graph
 
 
-def convert_graphs_to_numpy(graphs: dict) -> dict:
-    numpy_graphs = {}
-    for graph_name, graph in graphs.items():
-        numpy_graphs[graph_name] = convert_graph_to_numpy(graph)
-    return numpy_graphs
-
-
-def convert_numpy_to_umap(numpy_graph: tuple) -> np.ndarray:
-    adjacency_matrix, _ = numpy_graph
+def convert_numpy_to_umap(graph: nx.DiGraph) -> np.ndarray:
+    #TODO calculate adjacency matrix from graph
     umap_embedding = UMAP(n_components=2, metric='precomputed').fit_transform(adjacency_matrix)
     return umap_embedding
 
