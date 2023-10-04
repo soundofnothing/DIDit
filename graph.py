e6f27c552d0e8bd70d82681aa21ea5836faf7d32
soundofnothing <144745171+soundofnothing@users.noreply.github.com>
2023-10-04 01:24:01 -0400

    Update graph.py

--- graph.py
+++ graph.py
@@ -2,37 +2,38 @@ import networkx as nx
 import numpy as np
 from sklearn.feature_extraction.text import CountVectorizer, TfidfVectorizer
 from signature import Fingerprint
+from typing import Dict
 from umap import UMAP
 
 
-def create_char_graph(text: str) -> nx.DiGraph:
+def create_frequency_graph(frequencies: Dict[str, int]) -> nx.DiGraph:
+    graph = nx.DiGraph()
+    for datapoint, frequency in frequencies.items():
+        graph.add_node(datapoint, frequency=frequency)
+    return graph
+
+def create_char_graph_embedding(text: str) -> nx.DiGraph:
     fingerprint = Fingerprint.from_text(text)
     char_graph = nx.DiGraph()
     normed_char_graph = nx.DiGraph()
     
     # Add nodes to the graph
-    for character, frequency in fingerprint.NORMALIZED_CHARACTER_FREQUENCY.items():
-        normed_char_graph.add_node(character, frequency=frequency)
-
-    for character, frequency in fingerprint.CHARACTER_FREQUENCY.items():
-        char_graph.add_node(character, frequency=frequency)
+    normed_char_graph = create_frequency_graph(fingerprint.NORMALIZED_CHARACTER_FREQUENCY.items())
+    char_graph = create_frequency_graph(fingerprint.CHARACTER_FREQUENCY.items())
     
-    return nx.tensor_product(normed_char_graph, char_graph)
+    return nx.tensor_product(normed_char_graph, nx.union(normed_char_graph, char_graph))
 
 
-def create_word_graph(text: str) -> nx.DiGraph:
+def create_word_graph_embedding(text: str) -> nx.DiGraph:
     fingerprint = Fingerprint.from_text(text)
     word_graph = nx.DiGraph()
     normed_word_graph = nx.DiGraph()
     
     # Add nodes to the graph
-    for word, frequency in fingerprint.WORD_FREQUENCY.items():
-        word_graph.add_node(word, frequency=frequency)
-
-    for word, frequency in fingerprint.NORMALIZED_WORD_FREQUENCY.items():
-        normed_word_graph.add_node(word, frequency=frequency)
-    # todo investigate adding negative frequencies
-    return nx.tensor_product(normed_word_graph, word_graph)
+    normed_word_graph = create_frequency_graph(fingerprint.NORMALIZED_WORD_FREQUENCY.items())
+    word_graph = create_frequency_graph(fingerprint.WORD_FREQUENCY.items())
+    
+    return nx.tensor_product(normed_word_graph, nx.union(normed_word_graph, word_graph))
 
 
 def create_stopword_nonletter_graph(text: str) -> nx.DiGraph:
