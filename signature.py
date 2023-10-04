0190cac06540905f2349acabf6060358e9e8c87e
soundofnothing <144745171+soundofnothing@users.noreply.github.com>
2023-10-04 17:17:03 -0400

    Update signature.py

--- signature.py
+++ signature.py
@@ -10,25 +10,37 @@ def normalize_text(text: str) -> str:
     text = TextBlob(text)
     # remove whitespace, covert to lowercase, attempt to correct spelling
     text = text.strip().lower().correct()
-    # convert every word in a sentence to singular form
+    # convert every word in a sentence to singular form, this also should remove punctuation
     return ' '.join(word.singularize() for word in text.words)
 
 
-def calculate_character_frequencies(text: str) -> Dict[str, int]:
-    # do NOT convert to a blob, the punctuation would be stripped and we need it for a baseline
+def calculate_character_frequencies(text: str) -> Dict[str, float]:
+    # Calculate the total number of characters in the text
+    total_characters = len(text)
+    
+    # Calculate character frequencies and divide by the total number of characters
     character_counts = Counter(text)
-    return dict(character_counts)
+    character_frequencies = {char: count / total_characters for char, count in character_counts.items()}
+    
+    return character_frequencies
 
 
 def calculate_normalized_character_frequencies(text: str) -> Dict[str, int]:
     return calculate_character_frequencies(normalize_text(text))
 
 
-def calculate_word_frequencies(text: str) -> Dict[str, int]:
-    # do NOT convert to a blob, the underlying word frequencies would be changed and we need it for a baseline
+def calculate_word_frequencies(text: str) -> Dict[str, float]:
+    # Split the text into words
     words = text.split()
+    
+    # Calculate the total number of words in the text
+    total_words = len(words)
+    
+    # Calculate word frequencies and divide by the total number of words
     word_counts = Counter(words)
-    return dict(word_counts)
+    word_frequencies = {word: count / total_words for word, count in word_counts.items()}
+    
+    return word_frequencies
 
 
 def calculate_normalized_word_frequencies(text: str) -> Dict[str, int]:
@@ -82,15 +94,24 @@ def calculate_cosine_similarity_word(text: str) -> float:
 
 
 class Fingerprint(NamedTuple):
+    # character_delta := abs(character_freq - normed_character_freq)
     CHARACTER_FREQUENCY: Dict[str, int]
     NORMALIZED_CHARACTER_FREQUENCY: Dict[str, int]
+    
+    # word_delta := abs(word_freq - normed_word_freq)
     WORD_FREQUENCY: Dict[str, int]
     NORMALIZED_WORD_FREQUENCY: Dict[str, int]
+    
+    # measures the degree of character and word alignment with normed frequencies
     COSINE_SIMILARITY_CHAR: float
     COSINE_SIMILARITY_WORD: float
+
+    # structural_deviation := COSINE_SIMILARITY_CHAR * character_delta + COSINE_SIMILARITY_WORD * word_delta
     STOPWORD_FREQUENCY: Dict[str, int]
     NONLETTER_FREQUENCY: Dict[str, int]
 
+    # (character_delta, word_delta, and structural_deviation) is an "identity vector" for an author
+    
     @classmethod
     def from_text(cls, text: str):
         return cls(
@@ -136,4 +157,36 @@ def display_fingerprints(fingerprints, titles=None, rows=1, cols=None, figsize=(
             plt.title(wrapped_title)
 
     plt.tight_layout()
+    plt.show()
+
+
+# Define a function to pad character frequencies with zeros
+def pad_char_frequencies(fp, char_labels):
+    return [fp.NORMALIZED_CHARACTER_FREQUENCY.get(char, 0) for char in char_labels]
+
+
+def render_heatmap(text_snippets: List[str]):
+    import matplotlib.pyplot as plt
+    import textwrap
+    fingerprints = [Fingerprint.from_text(text) for text in text_snippets]
+
+    # Get the character labels from the first fingerprint
+    char_labels = list(fingerprints[0].NORMALIZED_CHARACTER_FREQUENCY.keys())
+
+    # Pad character frequencies for all fingerprints
+    char_freq_matrix = np.array([pad_char_frequencies(fp, char_labels) for fp in fingerprints])
+
+    # Define the labels for the x-axis (characters) and y-axis (text snippets)
+    snippet_labels = [textwrap.wrap(text, 20) for text in text_snippets]
+
+    # Create the heatmap
+    plt.figure(figsize=(12, 8))
+    sns.heatmap(char_freq_matrix, cmap="YlGnBu", xticklabels=char_labels, yticklabels=snippet_labels)
+
+    # Customize the heatmap appearance (e.g., add labels, title, etc.)
+    plt.xlabel("Character")
+    plt.ylabel("Text Snippet")
+    plt.title("Heatmap of Normalized Character Frequencies")
+
+    # Show the heatmap
     plt.show()
