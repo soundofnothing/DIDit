fcab75113dd4a18fee0fae5630a846c25d92ab87
soundofnothing <144745171+soundofnothing@users.noreply.github.com>
2023-10-08 22:32:11 -0400

    Update app.py

--- app.py
+++ app.py
@@ -1,81 +1,91 @@
 import dash
 import dash_core_components as dcc
 import dash_html_components as html
-import graph
-import timeline
-import umap.plot
 from dash.dependencies import Input, Output
-from visualize import visualize_character_frequencies, visualize_word_frequencies, visualize_stopword_and_nonletter_frequencies
+from visualize import render_heatmap, visualize_fingerprint_identity
+from signature import Fingerprint
 
+# Import dash_table.DataTable
+import dash_table
 
-def process_textual_data(rss_feed: timeline.RSSFeed) -> dict:
-    # Generate a stream of textual data from the RSS feed
-    timeseries = timeline.generate_timeline_timeseries(rss_feed)
+# Initialize the Dash app
+app = dash.Dash(__name__)
 
-    # Initialize a dictionary to store the graphs
-    graphs = {}
+# Define the layout of the app
+app.layout = html.Div([
+    html.H1("Text Analysis Dashboard"),
+    
+    # Input component for entering text snippets
+    dcc.Textarea(
+        id='text-input',
+        placeholder="Enter text snippets separated by newlines...",
+        style={'width': '100%', 'height': '200px'},
+    ),
+    
+    # Button to trigger analysis
+    html.Button('Analyze', id='analyze-button'),
+    
+    # Heatmap to display normalized character frequencies
+    dcc.Graph(id='heatmap'),
+    
+    # Identity visualization
+    html.Div(id='identity-visualization'),
 
-    # Process each entry in the timeseries
-    for entry in timeseries:
-        # Get the timestamped text from the entry
-        timestamp, text = entry
+    # DataTable for browsing fingerprint data
+    dash_table.DataTable(
+        id='fingerprint-table',
+        columns=[
+            {"name": "Character Frequency", "id": "CHARACTER_FREQUENCY"},
+            {"name": "Normalized Character Frequency", "id": "NORMALIZED_CHARACTER_FREQUENCY"},
+            {"name": "Word Frequency", "id": "WORD_FREQUENCY"},
+            {"name": "Normalized Word Frequency", "id": "NORMALIZED_WORD_FREQUENCY"},
+            {"name": "Cosine Similarity (Char)", "id": "COSINE_SIMILARITY_CHAR"},
+            {"name": "Cosine Similarity (Word)", "id": "COSINE_SIMILARITY_WORD"},
+            {"name": "Stopword Frequency", "id": "STOPWORD_FREQUENCY"},
+            {"name": "Nonletter Frequency", "id": "NONLETTER_FREQUENCY"},
+            {"name": "Character Delta", "id": "character_delta"},
+            {"name": "Word Delta", "id": "word_delta"},
+            {"name": "Structural Deviation", "id": "structural_deviation"},
+        ],
+        style_table={'overflowX': 'scroll'},
+        style_cell={'minWidth': '100px'},
+        filter_action='native',
+        sort_action='native',
+        page_size=10,
+    ),
+])
 
-        # Create graphs from the text
-        character_graph = graph.create_character_graph(text)
-        word_graph = graph.create_word_graph(text)
-        stopword_punctuation_graph = graph.create_stopword_punctuation_graph(text)
+# Define callback to update heatmap and DataTable
+@app.callback(
+    [Output('heatmap', 'figure'), Output('fingerprint-table', 'data')],
+    Input('analyze-button', 'n_clicks'),
+    Input('text-input', 'value')
+)
+def update_heatmap_and_table(n_clicks, text_input):
+    if n_clicks and text_input:
+        text_snippets = text_input.split('\n')
+        fingerprints = [Fingerprint.from_text(text) for text in text_snippets]
+        
+        # Prepare data for the DataTable
+        fingerprint_data = []
+        for fingerprint in fingerprints:
+            fingerprint_data.append({
+                "CHARACTER_FREQUENCY": fingerprint.CHARACTER_FREQUENCY,
+                "NORMALIZED_CHARACTER_FREQUENCY": fingerprint.NORMALIZED_CHARACTER_FREQUENCY,
+                "WORD_FREQUENCY": fingerprint.WORD_FREQUENCY,
+                "NORMALIZED_WORD_FREQUENCY": fingerprint.NORMALIZED_WORD_FREQUENCY,
+                "COSINE_SIMILARITY_CHAR": fingerprint.COSINE_SIMILARITY_CHAR,
+                "COSINE_SIMILARITY_WORD": fingerprint.COSINE_SIMILARITY_WORD,
+                "STOPWORD_FREQUENCY": fingerprint.STOPWORD_FREQUENCY,
+                "NONLETTER_FREQUENCY": fingerprint.NONLETTER_FREQUENCY,
+                "character_delta": fingerprint.character_delta,
+                "word_delta": fingerprint.word_delta,
+                "structural_deviation": fingerprint.structural_deviation
+            })
+        
+        return render_heatmap(text_snippets), fingerprint_data
+    else:
+        return {}, []
 
-        # Store the graphs in the dictionary under the corresponding timestamp
-        graphs[timestamp] = {
-            "character": character_graph,
-            "word": word_graph,
-            "stopword_punctuation": stopword_punctuation_graph
-        }
-
-    return graphs
-
-
-def visualize_umap_embedding(embedding: any) -> dcc.Graph:
-    # Example visualization using umap.plot.points
-    figure = umap.plot.points(embedding)
-    return dcc.Graph(figure=figure)
-
-
-def visualize_word_graph(word_graph: any) -> dcc.Graph:
-    # Example visualization using graph.create_word_graph
-    figure = graph.create_word_graph(word_graph)
-    return dcc.Graph(figure=figure)
-
-
-if __name__ == "__main__":
-    # Example usage
-    rss_feed = timeline.RSSFeed.ACCOUNT1
-    graphs = process_textual_data(rss_feed)
-
-    # Create Dash app
-    app = dash.Dash(__name__)
-
-    # Set up Dash app layout
-    app.layout = html.Div([
-        dcc.Graph(id="character-frequencies", figure=visualize_character_frequencies(graphs)),
-        dcc.Graph(id="word-frequencies", figure=visualize_word_frequencies(graphs)),
-        dcc.Graph(id="stopword-nonletter-frequencies", figure=visualize_stopword_and_nonletter_frequencies(graphs)),
-        dcc.Graph(id="umap-embedding", figure=visualize_umap_embedding(graph.convert_numpy_to_umap(graph.convert_graphs_to_numpy(graphs))))
-    ])
-
-    @app.callback(
-        Output("word-frequencies", "figure"),
-        [Input("character-frequencies", "hoverData")]
-    )
-    def update_word_graph(hoverData):
-        if hoverData is not None:
-            timestamp = hoverData["points"][0]["x"]
-            word_graph = graphs.get(timestamp, {}).get("word", None)
-
-            if word_graph is not None:
-                return visualize_word_graph(word_graph)
-
-        return None
-
-    # Run the Dash app
+if __name__ == '__main__':
     app.run_server(debug=True)
